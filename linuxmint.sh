e35a9ef4c324e37c24276563bbfc764717333e3b
Jokin <99480973+JOKITON@users.noreply.github.com>
2024-04-12 16:33:56 +0200

    Update linuxmint.sh, v2 name produces errors when executing.

--- linuxmint.sh
+++ linuxmint.sh
@@ -15,13 +15,13 @@ sudo apt install -y apt-transport-https ca-certificates curl gnupg
 sudo apt install -y docker docker-doc docker-registry
 
 # Get latest docker compose, v2
-sudo curl  -L "https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker\ compose
+sudo curl  -L "https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 
 # Make docker compose executable, give it right permissions
-sudo chmod +x /usr/local/bin/docker\ compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Check docker compose version
-docker\ compose --version
+docker-compose --version
 
 sudo apt install -y docker.io
 
